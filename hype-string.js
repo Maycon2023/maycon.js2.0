8b40999c3fc0af322b0d09670b39f96fa44314cd
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-09-06 09:07:50 -0300

    Add files via upload

---
+++ hype-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo1';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "120406";
+
+const citacao = "Meu nome é ";
+const meuNome = "Mycon";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// templante string ou template literal
