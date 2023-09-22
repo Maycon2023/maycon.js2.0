37f829d5aa2ad70fe3bd5b50c8748514e78ca833
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-09-22 09:58:10 -0300

    Add files via upload

---
+++ console.js
@@ -0,0 +1,11 @@
+// log -> registro
+
+const minhaVar = true;
+
+// console.log(245)
+// console.log(" eu sou seu pai")
+// console.log(minhaVar)
+
+// tratamento de erro!
+
+console.log('deu erro!')
