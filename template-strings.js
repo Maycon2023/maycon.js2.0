fe1a06cc88c3f253d3c75304cfc44d03b4e549db
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-10-03 11:43:27 -0300

    Add files via upload

--- template-strings.js
+++ template-strings.js
@@ -1,12 +1,9 @@
-const nome = "meram";
-const idade 10000-1000
+const nome = "tico";
+const sobrenome = "meram"
+const idade = 2023-2006;
+const objetivo =" comer o PedroH"
 const cidadeDeNascimento = " Guatiriba do Sul"
 
-// const apresentacao = "tico " + nome + ", minha idade é " + idade + " e te comi na cidade de " + cidadeDeNascimento;
-
-const apresentacao ="meu nome é xxx, minha idade é xxx e nasci na cidade de xxx"
+const apresentacao = "meu nome é "  + nome + sobrenome + ", minha idade é " + idade + " e te comi na cidade de " + cidadeDeNascimento + " e meu objetivo é " + objetivo;
 
 console.log(apresentacao)
-
-
-
