c82da0ba126bc1ae9e5ac8e7eb1bd1855f5209be
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-10-10 11:46:29 -0300

    Add files via upload

--- funcoes.js
+++ funcoes.js
@@ -6,15 +6,21 @@
 
 
 // 1) declara a função
-                       //string
+                      //2 -> valor de soma()
 function imprimeTexto(texto) {
     console.log(texto)
 }
-
 // 2) executa a função (1 ou + vezes)
 
-imprimeTexto("oi");
-imprimeTexto("outro texto");
+ imprimeTexto(soma());
+// imprimeTexto("outro texto");
 
 // três formas de escrever funções
 
+function soma(){
+    // outros códigos
+    // vários console.log()
+    return 111- 120;
+}
+
+// console.log(soma())
